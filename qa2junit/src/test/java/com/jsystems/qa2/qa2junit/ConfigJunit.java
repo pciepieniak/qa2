4ec809ee5f3717b9cabcfdb55b2e520b5277ba86
pciepieniak <pciepieniak@fmlogistic.com>
2019-06-11 10:02:40 +0200

    Junit parametrerized tests

---
+++ qa2junit/src/test/java/com/jsystems/qa2/qa2junit/ConfigJunit.java
@@ -0,0 +1,44 @@
+package com.jsystems.qa2.qa2junit;
+
+import org.junit.jupiter.api.*;
+
+public class ConfigJunit {
+    @BeforeAll
+    public static void beforeAll(TestInfo testInfo)
+    {
+        // Przed wywolaniem metod
+        System.out.println("-----------------------------------BEFORE-----------------------------------");
+        System.out.println("Test class = " + testInfo.getTestClass().getClass().getSimpleName());
+        System.out.println("Test name = " + testInfo.getDisplayName());
+    }
+
+    @AfterAll
+    public static void afterAll(TestInfo testInfo)
+    {
+        // Po wszystkich metodach
+        System.out.println("-----------------------------------AFTER-----------------------------------");
+        System.out.println("Test class = " + testInfo.getTestClass().getClass().getSimpleName());
+        System.out.println("Test name = " + testInfo.getDisplayName());
+    }
+
+    @BeforeEach
+    public void beforeEach(TestInfo testInfo)
+    {
+        // Przed kazda jedna metoda
+        System.out.println("-----------------------------------BEFORE EACH-----------------------------------");
+        System.out.println("Test class = " + testInfo.getTestClass().getClass().getSimpleName());
+        System.out.println("Test method = " + testInfo.getTestMethod());
+        System.out.println("Test name = " + testInfo.getDisplayName());
+    }
+
+
+    @AfterEach
+    public void afterEach(TestInfo testInfo)
+    {
+        // Po kazdej jednej metodzie
+        System.out.println("-----------------------------------AFTER EACH-----------------------------------");
+        System.out.println("Test class = " + testInfo.getTestClass().getClass().getSimpleName());
+        System.out.println("Test method = " + testInfo.getTestMethod());
+        System.out.println("Test name = " + testInfo.getDisplayName());
+    }
+}
--- qa2junit/src/test/java/com/jsystems/qa2/qa2junit/ParametrizedTest.java
+++ qa2junit/src/test/java/com/jsystems/qa2/qa2junit/ParametrizedTest.java
@@ -1,20 +1,23 @@
 package com.jsystems.qa2.qa2junit;
 
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Parametrized tests")
-public class ParametrizedTest {
+public class ParametrizedTest extends ConfigJunit{
 
     @ParameterizedTest(name = "Result for a test 1 with argument = {0}")
     @DisplayName("First test with parametr")
     @ValueSource(ints = {5, 15, 25})
     public void paramFirstTest (int number)
     {
+
         assertTrue(number % 5 == 0);
     }
 
@@ -24,5 +27,23 @@ public class ParametrizedTest {
     public void paramSecondTest (String value)
     {
         assertTrue(value.contains("Hello"));
+    }
+
+    @DisplayName("CSV value parametrized test")
+    @ParameterizedTest(name = "Parametrized test with values name {0} and value {1}")
+    @CsvSource(value =  {"Hello, 5", "HelloJunit 5, 15", "'Hello 5!', 25"}, delimiter = ',')
+    public void paramMultiArgTest(String param1, int param2)
+    {
+        assertTrue(param1.contains("Hello"));
+        assertTrue(param2 % 5 == 0);
+    }
+
+    @DisplayName("CSV file parametrized test")
+    @ParameterizedTest(name = "Parametrized test with data from csv file,  name {0} and value {1}")
+    @CsvFileSource(resources = "/plik.csv", delimiter = ',')
+    public void csvFileSourceTest(String param1, int param2)
+    {
+        assertTrue(param1.contains("Hello"));
+        assertTrue(param2 % 5 == 0);
     }
 }
